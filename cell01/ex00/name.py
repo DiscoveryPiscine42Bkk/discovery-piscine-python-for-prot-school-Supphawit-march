c436d2921fb839617ed14865210a9467a05e7f54
Supphawit-march <marchwork288@gmail.com>
2025-03-10 13:55:46 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name= "Supphawit"
+last_name= "Phupan"
+print(first_name , last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name= "Supphawit"
+last_name= "Phupan"
+whole_name=first_name + " " +last_name
+print(whole_name)
