f2a4b9fd6c7c35742a009671a35ff00121f616d7
Supphawit-march <marchwork288@gmail.com>
2025-03-11 13:47:56 +0700

    Delete cell03/ex03/advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++
@@ -1,9 +0,0 @@
-multiplier = 0
-
-while multiplier <= 10:
-    print(f"Table de {multiplier}:", end=" ")
-    i = 0
-    while i <= 10:
-        print(multiplier * i, end=" " if i < 10 else "\n")
-        i += 1
-    multiplier += 1
