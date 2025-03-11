00ff6b3b8c26bee67f309ff1aaf6be49b4958ed4
Supphawit-march <marchwork288@gmail.com>
2025-03-11 12:58:51 +0700

    Add files via upload

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,5 @@
+import math
+number = input("Give me a number: ")
+number = float(number)
+rounded_number = math.ceil(number)
+print(rounded_number)
---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,5 @@
+def swap_case(text):
+    return text.swapcase()
+user_input = input("Enter the word: ")
+result = swap_case(user_input)
+print("", result)
