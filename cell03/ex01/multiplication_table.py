009526bd302aa0f293e2c222856363931560fe86
Supphawit-march <marchwork288@gmail.com>
2025-03-11 13:48:11 +0700

    Add files via upload

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,8 +1,8 @@
 def multiplication_table(number):
     print(f"multiplication {number}")
-    for i in range(1, 13):
+    i = 1 
+    while i <=12:
         print(f"{number} x {i} = {number * i}")
-
-# กำหนดแม่สูตรคูณที่ต้องการ
+        i += 1
 number = int(input("Enter the number: "))
 multiplication_table(number)
